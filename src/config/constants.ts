47596c4198a996e31d4b9d48c6cf03d8ac7d5a40
ju90 <wangju18@h-partners.com>
2026-05-14 10:39:39 +0800

    refactor(knowledge)：提取公网地址到配置文件中
    Signed-off-by: ju90 <wangju18@h-partners.com>

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -16,3 +16,8 @@ export { TimeConstants, NetworkConstants } from './network';
 
 // Skills 相关常量
 export { SkillsApiConstants, AGENT_SKILLS_CONFIG } from './skills';
+
+// Knowledge相关常量配置
+export const KnowledgeConfig = {
+  KNOWLEDGE_URL: 'https://cn.devecostudio.huawei.com/codeGenie/bigSearch'
+};
