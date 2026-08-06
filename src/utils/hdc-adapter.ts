7c8bb4a8a039e7309cec7c7027d6912d22ea8c22
Mlesaa <fulongbin1@huawei.com>
2026-08-06 11:33:01 +0800

    Revert "feat(hdc-adapter)：添加 wakeUpScreen 方法，保证应用安装时屏幕为亮屏状态"
    This reverts commit cc423a6e95f9d92500fdb3d101c40f755d379a30.
    
    Signed-off-by: Mlesaa <fulongbin1@huawei.com>

--- src/utils/hdc-adapter.ts
+++ src/utils/hdc-adapter.ts
@@ -119,7 +119,6 @@ export class HdcAdapter {
     bundleName: string,
     mainAbility: string
   ): Promise<string> {
-    await this.wakeUpScreen(target);
     const args = [
       '-t',
       target,
@@ -137,9 +136,5 @@ export class HdcAdapter {
   public async forceStopApp(target: string, bundleName: string): Promise<string> {
     const args = ['-t', target, 'shell', 'aa', 'force-stop', bundleName];
     return await this.runHdc(args, false);
-  }
-
-  public async wakeUpScreen(target: string): Promise<string> {
-    return await this.runHdc(['-t', target, 'shell', 'power-shell', 'wakeup'], false);
   }
 }
